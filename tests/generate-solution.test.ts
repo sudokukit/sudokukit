0d80438329185a58f5725d1ffe720fe7fb1ccb77
Coen Mooij <coenmooij@gmail.com>
2025-08-14 14:27:19 +0200

    remove performance test overhead

--- tests/generate-solution.test.ts
+++ tests/generate-solution.test.ts
@@ -10,13 +10,12 @@ describe('GenerateSolution', () => {
 
   test(`measures average performance of generate over 50000 calls`, () => {
     const numberOfIterations = 50000;
-    let totalTime: number = 0;
+    const start: number = performance.now();
     for (let i: number = 0; i < numberOfIterations; i++) {
-      const start: number = performance.now();
       generateSolution();
-      const end: number = performance.now();
-      totalTime += end - start;
     }
+    const end: number = performance.now();
+    const totalTime: number = end - start;
     const average: number = totalTime / numberOfIterations;
     const microseconds: number = Math.round(average * 1000);
     console.log(`Average time: ${microseconds} µs`);
